e7fb7b873eb19dd354a2239a280a4395617c7f0e
mayorov <may.viktor@gmail.com>
2018-05-04 23:37:01 +0300

    current user

---
+++ controllers/user.js
@@ -0,0 +1,14 @@
+import * as UserService from '../services/UserService'
+
+export async function getCurrentUser(req, res, next) {
+    const { token } = req
+    try {
+        var user = await UserService.getUserByToken(token)
+    } catch ({message}) {
+        return next({
+            status: 500,
+            message
+        })
+    }
+    return res.json(user)
+}
